23aa7cf88410dc180190b85ff1b1acc6ec0c388c
Stefan Widgren <stefan.widgren@gmail.com>
2014-09-24 20:43:14 +0200

    More index plan test cases

--- tests/index_plan.R
+++ tests/index_plan.R
@@ -29,7 +29,54 @@ ip_1_exp <- structure(list(data = 12L, text = c(3L, 9L),
                                col = c(3L, 9L), wdf = c(1L, 1L)),
                                .Names = c("lbl", "col", "wdf")), id = 1L),
                       .Names = c("data", "text", "prefix", "id"))
-ip_1_obs <- xapr:::index_plan(data ~ S*TITLE + X*DESCRIPTION + Q:id_NUMBER,
-                              cols)
-str(ip_obs)
+ip_1_obs <- xapr:::index_plan(data ~ S*TITLE + X*DESCRIPTION + Q:id_NUMBER, cols)
+str(ip_1_obs)
 stopifnot(identical(ip_1_obs, ip_1_exp))
+
+##
+## Test case 2
+##
+ip_2_exp <- structure(list(data = NULL, text = 1:12,
+                           prefix = structure(list(lbl = character(0),
+                               col = integer(0), wdf = integer(0)),
+                               .Names = c("lbl", "col", "wdf")), id = NULL),
+                      .Names = c("data", "text", "prefix", "id"))
+ip_2_obs <- xapr:::index_plan(~., cols)
+str(ip_2_obs)
+stopifnot(identical(ip_2_obs, ip_2_exp))
+
+##
+## Test case 3
+##
+ip_3_exp <- structure(list(data = NULL, text = 1:12,
+                           prefix = structure(list(lbl = character(0),
+                               col = integer(0), wdf = integer(0)),
+                               .Names = c("lbl", "col", "wdf")), id = NULL),
+                      .Names = c("data", "text", "prefix", "id"))
+ip_3_obs <- xapr:::index_plan(~.+data, cols)
+str(ip_3_obs)
+stopifnot(identical(ip_3_obs, ip_3_exp))
+
+##
+## Test case 4
+##
+ip_4_exp <- structure(list(data = NULL, text = 1:12,
+                           prefix = structure(list(lbl = character(0),
+                               col = integer(0), wdf = integer(0)),
+                               .Names = c("lbl", "col", "wdf")), id = NULL),
+                      .Names = c("data", "text", "prefix", "id"))
+ip_4_obs <- xapr:::index_plan(~data+., cols)
+str(ip_4_obs)
+stopifnot(identical(ip_4_obs, ip_4_exp))
+
+##
+## Test case 4
+##
+ip_5_exp <- structure(list(data = NULL, text = 1:12,
+                           prefix = structure(list(lbl = character(0),
+                               col = integer(0), wdf = integer(0)),
+                               .Names = c("lbl", "col", "wdf")), id = 1L),
+                      .Names = c("data", "text", "prefix", "id"))
+ip_5_obs <- xapr:::index_plan(~Q:id_NUMBER+., cols)
+str(ip_5_obs)
+stopifnot(identical(ip_5_obs, ip_5_exp))
